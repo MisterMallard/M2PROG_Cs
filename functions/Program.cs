4f78d49adec869b321ff9db6e65d90173f623916
Calvin Hewitt <40927@ma-web.nl>
2025-11-18 13:04:00 +0100

    functions 5 questions

--- functions/Program.cs
+++ functions/Program.cs
@@ -7,8 +7,50 @@ internal class Program // de function heet program //en we gebruikt de program o
         Program program = new Program(); // we hebben een varible van program gemaakt //varible; program
         program.Run(); 
     }
-    internal string Run()
+    internal void Run()
     {
         Console.WriteLine("Dit is nu de start van mijn programma");
+        Console.WriteLine("Ik ga nu een vraag stellen");
+        Vraag1();
+        Vraag2();
+        Vraag3();
+        Vraag4();
+        Vraag5();
+        Console.WriteLine("Dat waren alle vragen");
+    }
+    internal void Vraag1()
+    {
+        Console.WriteLine("How long do you think you'd survive in a zombie apocalypse?");
+        string antwoord = Console.ReadLine();
+
+        Console.WriteLine("one day");
+    }
+    internal void Vraag2()
+    {
+        Console.WriteLine("Wat is je lievelings eten?");
+        string antwoord = Console.ReadLine();
+
+        Console.WriteLine("Pizza");
+    }
+    internal void Vraag3()
+    {
+        Console.WriteLine("Wat is je lievelings kleur?");
+        string antwoord = Console.ReadLine();
+
+        Console.WriteLine("Blauw");
+    }
+    internal void Vraag4()
+    {
+        Console.WriteLine("Wat is je lievelings dier?");
+        string antwoord = Console.ReadLine();
+
+        Console.WriteLine("Vos");
+    }
+    internal void Vraag5()
+    {
+        Console.WriteLine("Wat is je lievelings land?");
+        string antwoord = Console.ReadLine();
+
+        Console.WriteLine("Amerika");
     }
 }
