655bd27432307c119d6df045ed3c970f538f1d5c
Calvin Hewitt <40927@ma-web.nl>
2025-11-18 12:49:52 +0100

    i hate functions

---
+++ functions/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace functions;
+
+internal class Program // de function heet program //en we gebruikt de program om de function aan te roepen
+{
+    static void Main(string[] args)
+    {
+        Program program = new Program(); // we hebben een varible van program gemaakt //varible; program
+        program.Run(); 
+    }
+    internal string Run()
+    {
+        Console.WriteLine("Dit is nu de start van mijn programma");
+    }
+}
